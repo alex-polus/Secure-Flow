8d70e7bb8698e82adec1c4e58cc44869e63424af
Hydral8 <sbae703@gmail.com>
2021-05-09 03:31:55 -0700

    changed stuff

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import LandingPage from "./landingpage";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import "./styles.css";
-
+import Login from "./Login";
+import "./css/bootstrap.min.css";
+import "./css/cover.css";
+import Signup from "./Signup";
 
 function App() {
   return (
     <div className="App">
-      <h1>Validated Login Form</h1>
-      <LandingPage/>
+      <Router>
+        <Route exact path="/">
+          <LandingPage />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/signup">
+          <Signup />
+        </Route>
+      </Router>
     </div>
   );
 }
--- src/landingpage.js
+++ src/landingpage.js
@@ -1,38 +1,44 @@
 import React, { Component } from "react";
-import './css/bootstrap.min.css'
-import './css/cover.css'
-import logo from './css/logo.png'
+import { Link } from "react-router-dom";
+import "./css/bootstrap.min.css";
+import "./css/cover.css";
+import logo from "./css/logo.png";
 
 class LandingPage extends Component {
   render() {
     return (
-    <div className="text-center">
-      <div className="cover-container d-flex h-100 p-3 mx-auto flex-column">
-        <header className="masthead mb-auto" id="hello"></header>
-        <img src= {logo} />
-        <main role="main" className="inner cover">
-          <h1 className="cover-heading">Privacy in your control</h1>
-          <p className="lead">
-            Secure-flow was designed for users to have control of their own
-            data, protects privacy and prevent organizations from using your
-            data without permission
-          </p>
-
-          <a className="btn btn-lg btn-secondary">Sign in</a>
-          <a className="btn btn-lg btn-secondary">Sign Up</a>
-        </main>
-
-        <footer className="mastfoot mt-auto">
-          <div className="inner">
-            <p>
-              2021 TOhacks
-              <a href="https://devpost.com/software/secureflow"> Devpost</a>
+      <div className="text-center">
+        <div className="cover-container d-flex h-100 p-3 mx-auto flex-column">
+          <header className="masthead mb-auto" id="hello"></header>
+          <img src={logo} />
+          <main role="main" className="inner cover">
+            <h1 className="cover-heading">Privacy in your control</h1>
+            <p className="lead">
+              Secure-flow was designed for users to have control of their own
+              data, protects privacy and prevent organizations from using your
+              data without permission
             </p>
-          </div>
-        </footer>
+
+            <Link to="/login">
+              <a className="btn btn-lg btn-secondary">Sign in</a>
+            </Link>
+            <Link to="/signup">
+              <a className="btn btn-lg btn-secondary">Sign Up</a>
+            </Link>
+          </main>
+
+          <footer className="mastfoot mt-auto">
+            <div className="inner">
+              <p>
+                2021 TOhacks
+                <a href="https://devpost.com/software/secureflow"> Devpost</a>
+              </p>
+            </div>
+          </footer>
+        </div>
       </div>
-    </div>);
+    );
   }
 }
 
-export default LandingPage
+export default LandingPage;
